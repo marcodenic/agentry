57863ceb6ac2587d8f4e9e5cffa1e8f32dfff48d
Marco De Nichilo <marcode@gmail.com>
2025-09-05 22:40:24 +0100

    chore: bump version to 0.1.2

--- packaging/homebrew/agentry.rb
+++ packaging/homebrew/agentry.rb
@@ -1,7 +1,7 @@
 class Agentry < Formula
   desc "Minimal, performant AI-Agent runtime"
   homepage "https://github.com/marcodenic/agentry"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     url "https://github.com/marcodenic/agentry/releases/download/v#{version}/agentry-darwin-amd64"
